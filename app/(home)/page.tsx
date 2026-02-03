b65e64d22068a7e0249a11ccd14efad63716b8b7
Federico Di Francesco <federico.difrancesco@alkemy.com>
2026-02-03 12:29:24 +0100

    First commit! Goooo

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Searchbar from "./components/Searchbar";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <p>Homepage!</p>
+        <Searchbar />
+      </main>
+    </div>
+  );
+}
---
+++ app/games/[[...id]]/page.tsx
@@ -0,0 +1,9 @@
+export default function Games() {
+  return (
+    <div>
+      <main>
+        <p>Games page!</p>
+      </main>
+    </div>
+  );
+}
